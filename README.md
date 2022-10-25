a2849b71d84476f9c33d70508d0b883e6c4f6735
Larson Collier <collierlarson@gmail.com>
2022-10-25 18:25:10 -0400

    2nd attmpt. Should have readme update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# My Personal Website
+# The Little Swan that Goosed
+
+Some swans don't want to be swans. *Some* swans, such as the one on this site, have always longed to be **geese**.
+
+By mousing over this forelorn swan you can make its dreams come **true!**
+
+## Why
+My screenname has been `swan_the_goose` for some time now, so for my personal site I wanted to explore the concepts I've learned during the pre-work to flesh out this name.
+
+[**LinkedIn**](https://www.linkedin.com/in/larson-collier/)
--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const h2 = document.createElement("h2");
 h2.textContent = "..a website by Larswan Collider";
 
 document.querySelector("body").appendChild(h2);
+
+const swan = document.getElementById("swan");
+const swanSrc = "https://previews.123rf.com/images/nataba/nataba1712/nataba171200045/91734319-portrait-of-a-beautiful-bird-swan-standing-on-an-isolated-white-background.jpg";
+const gooseSrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Birds_Illustrated_Canada_Goose_white_background.jpg/701px-Birds_Illustrated_Canada_Goose_white_background.jpg?20161103112757";
+
+
+document.addEventListener('mouseover', toGoose());
+
+//i think these are HTML events
+//document.swan.onmouseover= toGoose(swan);
+//document.swan.onmouseout= toSwan(swan);
+
+function toGoose(img){
+     img.setAttribute("src", `${gooseSrc}`);
+   // return "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Birds_Illustrated_Canada_Goose_white_background.jpg/701px-Birds_Illustrated_Canada_Goose_white_background.jpg?20161103112757"
+    }
+
+    
+function toSwan(img){
+    img.setAttribute("src", `${swanSrc}`)
+}
+*/
